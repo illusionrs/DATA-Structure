3cb1048b46c2f896271a54663d51ca8f11b30a23
illusionrs <36722623+illusionrs@users.noreply.github.com>
2020-04-11 14:08:44 +0530

    Create client.java

---
+++ BinaryTree/client.java
@@ -0,0 +1,23 @@
+package BinaryTree;
+
+public class client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 50 true 25 true 12 false false true 39 false false true 75 true 41 false false true 87 false false
+        BT tree=new BT();
+        tree.display();
+        tree.Mirror();
+        System.out.println("****************************");
+        tree.display();
+//        System.out.println(tree.size2());
+//        
+//        System.out.println();
+//        tree.preOrder();
+//        tree.inOrder();
+//        
+//        System.out.println(tree.max());
+	}
+
+}
+
